c0d702f57623785fc4944efa530e8fbf2299385f
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-06-04 15:59:33 +0300

    Added Data

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Agumon','2-3-2020', 10.23,'1',0);
+INSERT INTO animals (name,date_of_birth,weight_kg,neutered,escape_attempts) VALUES ('Gabumon','11-1-2018', 8,'1',2);
+INSERT INTO animals (name,date_of_birth,weight_kg,neutered,escape_attempts) VALUES ('Pikachu','1-7-2021', 15.04,'0',1);
+INSERT INTO animals (name,date_of_birth,weight_kg,neutered,escape_attempts) VALUES ('Devimon','5-12-2017', 11,'1',5);
