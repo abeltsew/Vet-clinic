3961c6591a0c23ded23492ed063b88a510411c11
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-06-05 11:12:50 +0300

    Add Records

--- data.sql
+++ data.sql
@@ -4,3 +4,12 @@ INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) 
 INSERT INTO animals (name,date_of_birth,weight_kg,neutered,escape_attempts) VALUES ('Gabumon','11-1-2018', 8,'1',2);
 INSERT INTO animals (name,date_of_birth,weight_kg,neutered,escape_attempts) VALUES ('Pikachu','1-7-2021', 15.04,'0',1);
 INSERT INTO animals (name,date_of_birth,weight_kg,neutered,escape_attempts) VALUES ('Devimon','5-12-2017', 11,'1',5);
+
+
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Charmander', '2-8-2020',-11,'0',0);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Plantmon', '11-15-2021',-5.7,'1',2);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Squirtle', '4-2-1993',-12.13,'0',3);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Angemon', '6-12-2005',-45,'1',1);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Boarmon', '6-7-2005',20.4,'1',7);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Blossom', '10-13-1998',17,'1',3);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Ditto', '5-14-2022',22,'1',4);
